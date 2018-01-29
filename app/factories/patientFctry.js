8373e182b05a4153eaf42486056b6914c9e8b225
Melissa Bell <melissabell456@gmail.com>
2018-01-29 09:03:40 -0600

    console.logging patient data which for availability to print based on route

--- app/factories/patientFctry.js
+++ app/factories/patientFctry.js
@@ -1,12 +1,20 @@
 "use strict"; 
 
-angular.module("DoctorApp").factory("PatientFctry", function ($scope, $routeParams, PatientFctry) {
-
-  console.log($routeParams);
-
-  PatientFctry.getPatients()
-  .then( (patientData) => {
-    console.log("you got it", patientData);
+angular.module("DoctorApp").factory("PatientFctry", function ($q, $http) {
+  
+    const getPatients = () => {
+      return $q( (resolve, reject) => {
+        $http
+        .get("https://fir-a5a79.firebaseio.com/patients.json")
+        .then( (patientData) => {
+          console.log(patientData.data);
+          resolve(patientData.data);
+        })
+        .catch( (err) => {
+          console.log(err);
+          reject(err);
+        });
+      });
+    };
+    return { getPatients };
   });
-
-});
